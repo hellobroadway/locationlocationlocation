c5722fe6421136e5bd06a06ab2ff7bdca3440f39
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2015-05-11 15:37:22 -0400

    :space_invader: filtering working

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var apiurl = "http://api.nytimes.com/svc/events/v2/listings.jsonp?filters=category:%28Theater%29,borough:Manhattan&api-key=249fd02b51b18fa0242178b3b4c32d39:10:67424442&limit=100&callback=?";
+var event_limit = 200;
+
+var apiurl = "http://api.nytimes.com/svc/events/v2/listings.jsonp?filters=category:%28Theater%29,borough:Manhattan&api-key=249fd02b51b18fa0242178b3b4c32d39:10:67424442&limit="+event_limit+"&callback=?";
 
 var attribution_mapbox = 'Map via <a href="http://openstreetmap.org">OpenStreetMap</a>, <a href="http://mapbox.com">Mapbox</a>';
 
@@ -13,14 +15,22 @@ var jsonurl = 'https://gist.githubusercontent.com/mgiraldo/cc86b6b043f3ad16a719/
 
 var overlays, geodata, geolayer;
 
-var mapdata, categories = [], subcategories = [];
+var mapdata = [], categories = [], subcategories = [];
 
-function showMap(geodata) {
-    geolayer = L.geoJson(geodata, {onEachFeature: showPopup});
-    // add the points to the map
-    geolayer.addTo(map);
-    // zoom the map to the bounds of the points
-    map.fitBounds(geolayer.getBounds());
+var geodata = [];
+
+function showMap() {
+    var i;
+    // console.log(mapdata);
+    var b = new L.LatLngBounds();
+    for (i=0;i<mapdata.length;i++) {
+        var geo = L.geoJson(mapdata[i], {onEachFeature: showPopup});
+        geodata.push(geo);
+        geo.addTo(map);
+        map.setView(geo.getBounds().getCenter(), 12);
+        b.extend(geo.getBounds());
+    }
+    map.fitBounds(b);
 }
 
 function showPopup(feature, layer) {
@@ -36,47 +46,87 @@ function showPopup(feature, layer) {
     layer.bindPopup(html);
 }
 
-function geoJSONify(resultarray) {
-  mapdata = {};
-  categories = [];
-  subcategories = [];
-  var i, j;
-  var result = {};
-  result.type = "FeatureCollection";
-  result.features = [];
-  var keys = [
-    // "event_id",
-    "event_name",
-    "event_detail_url",
-    "venue_name",
-    "venue_website",
-    "category",
-    "subcategory",
-    "free",
-    "kid_friendly"
-  ];
-  var l = resultarray.length;
-  for (i=0;i<l;i++) {
-    var item = resultarray[i];
-    feature = {};
-    feature.type = "Feature";
-    feature.properties = {};
-    for (j=0;j<keys.length;j++) {
-      var key = keys[j];
-      feature.properties[key] = item[key];
-      if (key == "category" && categories.indexOf(item[key])==-1) categories.push(item[key]);
-      if (key == "subcategory" && subcategories.indexOf(item[key])==-1) subcategories.push(item[key]);
+function buildFacets() {
+    var i;
+    var html;
+    var cat;
+    for (i=0; i<categories.length; i++) {
+        cat = categories[i];
+        html = '<option value="'+cat+'">' + cat + '</option>';
+        $("#categories").append(html);
     }
-    feature.geometry = {
-      type: "Point",
-      coordinates: [item.geocode_longitude,item.geocode_latitude]
+    for (i=0; i<subcategories.length; i++) {
+        cat = subcategories[i];
+        html = '<option value="'+cat+'">' + cat + '</option>';
+        $("#subcategories").append(html);
     }
-    result.features.push(feature);
-  }
-  return result;
 }
 
-$.getJSON(apiurl, function(data){
-  mapdata = geoJSONify(data.results);
-  showMap(mapdata);
-});
+function geoJSONify(resultarray) {
+    mapdata = {};
+    categories = [];
+    subcategories = [];
+    var i, j;
+    var result = [];
+    var keys = [
+        // "event_id",
+        "event_name",
+        "event_detail_url",
+        "venue_name",
+        "venue_website",
+        "category",
+        "subcategory",
+        "free",
+        "kid_friendly"
+    ];
+    var l = resultarray.length;
+    for (i=0;i<l;i++) {
+        var item = resultarray[i];
+        feature = {};
+        feature.type = "Feature";
+        feature.properties = {};
+        for (j=0;j<keys.length;j++) {
+            var key = keys[j];
+            feature.properties[key] = item[key];
+            if (key == "category" && categories.indexOf(item[key])==-1) categories.push(item[key]);
+            if (key == "subcategory" && subcategories.indexOf(item[key])==-1) subcategories.push(item[key]);
+        }
+        feature.geometry = {
+            type: "Point",
+            coordinates: [item.geocode_longitude,item.geocode_latitude]
+        }
+        result.push(feature);
+    }
+    return result;
+}
+
+function filterCategory(type, name) {
+    var i;
+    for (i=0;i<mapdata.length;i++) {
+        var item = mapdata[i];
+        var geo = geodata[i];
+        if (item.properties[type] != name) {
+            if (map.hasLayer(geo)) map.removeLayer(geo);
+        } else {
+            if (!map.hasLayer(geo)) map.addLayer(geo);
+        }
+    }
+}
+
+
+function init() {
+    $("#categories").on( "change", function (event) {
+        filterCategory("category", event.target.value);
+    });
+    $("#subcategories").on( "change", function (event) {
+        filterCategory("subcategory", event.target.value);
+    });
+    $.getJSON(apiurl, function(data){
+        // console.log(data);
+        mapdata = geoJSONify(data.results);
+        buildFacets();
+        showMap();
+    });
+}
+
+init();
